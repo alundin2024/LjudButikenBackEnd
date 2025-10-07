c7ac9a4a3a82be8fae174e54e200b21fcb25499e
Anton Lundin <lundin09@gmail.com>
2025-10-07 14:40:11 +0200

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LjudButikenBackEnd.Controllers;
+
+
+// api/Products
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Products> GetProducts()
+
+    {
+
+    }
+}
---
+++ Domain/Products.cs
@@ -0,0 +1,9 @@
+﻿namespace LjudButikenBackEnd.Domain;
+
+public class Products
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public DateTime? DeadLine { get; set; }
+}
